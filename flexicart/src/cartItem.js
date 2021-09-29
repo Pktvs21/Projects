bb8b39a663cb76bd4fc78c32ad8e2b6943daa5a0
Pavan Kumar .T <tvspk100@gmail.com>
2021-09-29 14:10:35 +0530

    Add styling to the Cart Component

---
+++ flexicart/src/cartItem.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export class cartItem extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
+
+export default cartItem

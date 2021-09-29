c331841bc44da8b64dd50b63447bc3a935380b81
Pavan Kumar .T <tvspk100@gmail.com>
2021-09-29 14:37:19 +0530

    Add cartItem Component

--- flexicart/src/cartItem.js
+++ flexicart/src/cartItem.js
@@ -1,13 +1,31 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 
-export class cartItem extends Component {
-    render() {
-        return (
-            <div>
-                
-            </div>
-        )
-    }
+class CartItem extends Component {
+  render() {
+    return (
+      <div className="cart-item">
+        <div className="left-block">
+          <img style={styles.image} />
+        </div>
+        <div className="right-block">
+          <div style={{ fontSize: 25 }}>Phone</div>
+          <div style={{ color: "#777" }}>Rs 999</div>
+          <div style={{ color: "#777" }}>Qty:2</div>
+          <div>
+            <div className="cart-item-actions">{/* Buttons */}</div>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
-export default cartItem
+const styles = {
+  image: {
+    height: 110,
+    width: 110,
+    borderRadius: 4,
+    backgroundColor: "#ccc",
+  },
+};
+export default CartItem;
